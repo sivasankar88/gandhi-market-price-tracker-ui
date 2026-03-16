4dd7f31a5b018a7267c44d1ef9b5425c004f639c
sivasankar88 <sivasankar8383@gmail.com>
2026-03-16 07:53:29 +0530

    feat: env variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ import type {
   TrendPoint,
 } from "../types";
 
-const BASE_URL = "http://localhost:8081";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
